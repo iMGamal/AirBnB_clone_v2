823c955604258b62240710041e161bdcd5c39f0e
iMGamal <mohamed.gamal126027@gmail.com>
2024-07-29 02:03:15 +0000

    Task 1: Handling another route

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -7,8 +7,14 @@ web_app = Flask(__name__)
 
 @web_app.route('/', strict_slashes=False)
 def welcome():
-    """Handle the initial route."""
+    """Handle initial route."""
     return "Hello HBNB!"
+
+
+@web_app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """Handle second route."""
+    return "HBNB"
 
 
 web_app.run(host='0.0.0.0', port=5000)
