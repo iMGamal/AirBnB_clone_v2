5dbc8d20d65e5ab365f41873e360490ec0411f29
iMGamal <mohamed.gamal126027@gmail.com>
2024-07-29 03:01:46 +0000

    Add AUTHORS

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+"""Module for flask server."""
+from flask import Flask, render_template
+
+web_app = Flask(__name__)
+
+
+@web_app.route('/', strict_slashes=False)
+def welcome():
+    """Handle initial route."""
+    return "Hello HBNB!"
+
+
+@web_app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """Handle second route."""
+    return "HBNB"
+
+
+@web_app.route('/c/<text>', strict_slashes=False)
+def c(text):
+    """Handle third route."""
+    formatted = text.replace('_', ' ')
+    return f"C {formatted}"
+
+
+@web_app.route('/python/<text>', strict_slashes=False)
+def python(text="is cool"):
+    formatted = text.replace('_', ' ')
+    return f"Python {formatted}"
+
+
+@web_app.route('/number/<n>', strict_slashes=False)
+def number(n):
+    if n.isdigit():
+        return f"{n} is a number"
+    return "NULL"
+
+
+@web_app.route('/number_template/<n>', strict_slashes=False)
+def render(n):
+    """Handle template route."""
+    return render_template('5-number.html', number=n)
+
+
+web_app.run(host='0.0.0.0', port=5000)
+
