4613fde2d44675e2ee51c7f8ffa9075a13b86d04
iMGamal <mohamed.gamal126027@gmail.com>
2024-07-29 02:39:43 +0000

    Tasks 3 & 4: Handling python and number routes

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Module for flask server."""
+from flask import Flask
+
+web_app = Flask(__name__)
+
+
+@web_app.route('/', strict_slashes=False)
+def welcome():
+    """Handle initial route."""
+    return "Hello HBNB!"
+
+
+@web_app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """Handle second route."""
+    return "HBNB"
+
+
+@web_app.route('/c/<text>', strict_slashes=False)
+def c(text):
+    """Handle third route."""
+    formatted = text.replace('_', ' ')
+    return f"C {formatted}"
+
+
+web_app.run(host='0.0.0.0', port=5000)
+
